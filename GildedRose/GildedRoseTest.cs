06584aaa02eeb6e1bec4473d9170d522280bf1e2
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-11 12:02:46 +0100

    Base tests all pass, no refactoring yet

---
+++ GildedRose/GildedRoseTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GildedRose
+{
+    [TestFixture()]
+    public class GildedRoseTest
+    {
+        [Test()]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            global::GildedRose.GildedRose app = new global::GildedRose.GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("foo", Items[0].Name);
+        }
+    }
+}
