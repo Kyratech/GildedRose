b5010a3b26a71cdbfabcc3ec4686706dc8e4f494
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-11 13:38:43 +0100

    Moved Item class into its own file

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -87,14 +87,4 @@ namespace GildedRose
         }
 
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-
-        public int SellIn { get; set; }
-
-        public int Quality { get; set; }
-    }
-
 }
