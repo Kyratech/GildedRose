2e52309f1cf049c0eea2a158fdb13f3505176c42
Matthew Langford <Mathew.Langford@softwire.com>
2017-07-11 13:10:15 +0100

    Added test for passes quality pattern.
    Also added pass item to over 50 test,
    so that we don't just get it working for brie

--- GildedRose/GildedRoseTest.cs
+++ GildedRose/GildedRoseTest.cs
@@ -115,17 +115,22 @@ namespace GildedRose
         public void QualityIsNeverMoreThan50()
         {
             Item brieItem = new Item { Name = "Aged Brie", SellIn = 10, Quality = 49 };
-            IList<Item> items = new List<Item> { brieItem };
+            Item passItem = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49 };
+            IList<Item> items = new List<Item> { brieItem, passItem };
 
             GildedRose app = new GildedRose(items);
 
             Assert.AreEqual(49, items[0].Quality);
+            Assert.AreEqual(49, items[1].Quality);
             app.UpdateQuality();
             Assert.AreEqual(50, items[0].Quality);
+            Assert.AreEqual(50, items[1].Quality);
             app.UpdateQuality();
             Assert.AreEqual(50, items[0].Quality);
+            Assert.AreEqual(50, items[1].Quality);
             app.UpdateQuality();
             Assert.AreEqual(50, items[0].Quality);
+            Assert.AreEqual(50, items[1].Quality);
         }
 
         [Test()]
@@ -160,6 +165,53 @@ namespace GildedRose
             Assert.AreEqual(10, items[0].Quality);
             app.UpdateQuality();
             Assert.AreEqual(10, items[0].Quality);
+        }
+
+        [Test()]
+        public void BackstagePassesQualityRamp()
+        {
+            Item passItem = new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 10 };
+            IList<Item> items = new List<Item> { passItem };
+
+            GildedRose app = new GildedRose(items);
+
+            //Increases by 1 until 10 days before
+            Assert.AreEqual(10, items[0].Quality);
+            app.UpdateQuality();//14 Days left
+            Assert.AreEqual(11, items[0].Quality);
+            app.UpdateQuality();//13
+            Assert.AreEqual(12, items[0].Quality);
+
+            app.UpdateQuality();//12
+            app.UpdateQuality();//11
+            app.UpdateQuality();//10
+
+            //Increases by 2 when less than 10 days left
+            Assert.AreEqual(15, items[0].Quality);
+            app.UpdateQuality();//9
+            Assert.AreEqual(17, items[0].Quality);
+            app.UpdateQuality();//8
+            Assert.AreEqual(19, items[0].Quality);
+
+            app.UpdateQuality();//7
+            app.UpdateQuality();//6
+            app.UpdateQuality();//5
+
+            //Increases by 3 when less than 5 days left
+            Assert.AreEqual(25, items[0].Quality);
+            app.UpdateQuality();//4
+            Assert.AreEqual(28, items[0].Quality);
+            app.UpdateQuality();//3
+            Assert.AreEqual(31, items[0].Quality);
+
+            app.UpdateQuality();//2
+            app.UpdateQuality();//1
+            app.UpdateQuality();//0
+
+            //Drop to zero after concert
+            Assert.AreEqual(40, items[0].Quality);
+            app.UpdateQuality();//-1 Days left (concert yesterday)
+            Assert.AreEqual(0, items[0].Quality);
         }
     }
 }
